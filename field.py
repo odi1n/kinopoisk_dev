b45da8147d40ef48899e7f7a69a2833456ea00c1
odi1n <sashf1999@mail.ru>
2022-09-30 14:53:43 +0300

    Вынес field отдельно

---
+++ field.py
@@ -0,0 +1,20 @@
+from enum import Enum
+
+
+class Field(Enum):
+    KP = 'id'
+    IMDB = 'externalId.imdb'
+    TMDB = 'externalId.tmdb'
+    YEAR = 'year'
+
+
+class Fields(Enum):
+    KP = 'id'
+    IMDB = 'externalId.imdb'
+    TMDB = 'externalId.tmdb'
+    YEAR = 'year'
+    NAME = 'name'
+    TYPE_NUMBER = 'typeNumber'
+    MOVIE_ID = 'movieId'
+    TYPE = 'type'
+    LANGUAGE = 'language'
--- type.py
+++
@@ -1,7 +0,0 @@
-class Field:
-    KP: str = 'id'
-    IMDB: str = 'externalId.imdb'
-    TMDB: str = 'externalId.tmdb'
-    YEAR: str = 'year'
-    NAME: str = 'name'
-    TYPE_NUMBER: str = 'typeNumber'
