d6e16d58f9292257cb40f12737c944edba45e459
odi1n <sashf1999@mail.ru>
2023-03-04 01:00:38 +0300

    Добавил пример для movie/{id}

---
+++ example/example_movie.py
@@ -0,0 +1,19 @@
+from kinopoisk_dev import IdField, KinopoiskDev
+
+kp = KinopoiskDev(token=TOKEN)
+
+
+def main():
+    return kp.find_one_movie(666)
+
+
+async def amain():
+    return await kp.afind_one_movie(666)
+
+
+if __name__ == "__main__":
+    item = main()
+    print(item)
+
+    item = asyncio.run(amain())
+    print(item)
