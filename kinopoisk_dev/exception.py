b70a0315eb97b4fe7f264d36a21eb712ce071bec
odi1n <sashf1999@mail.ru>
2023-03-11 00:28:53 +0300

    Поправил exception

--- kinopoisk_dev/exception.py
+++ kinopoisk_dev/exception.py
@@ -1,12 +1,12 @@
 class ApiUnauthenticated(Exception):
     def __init__(self, response_status, response_text):
-        super(ApiUnauthenticated, self).__init__(response_status)
+        super().__init__(response_status)
         self.response_text = response_text
 
 
 class ApiNotFound(Exception):
     def __init__(self, response_status, response_text):
-        super(ApiNotFound, self).__init__(response_status)
+        super().__init__(response_status)
         self.response_text = response_text
 
 
