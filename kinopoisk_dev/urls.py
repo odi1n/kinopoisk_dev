c0c52e2a27ea88a6a0426b42a673263f7030b2ad
odi1n <sashf1999@mail.ru>
2023-03-12 19:11:32 +0300

    Добавил новые ссылки

--- kinopoisk_dev/urls.py
+++ kinopoisk_dev/urls.py
@@ -1,8 +1,20 @@
-LINK = "https://api.kinopoisk.dev/v1/"
-RANDOM = f"{LINK}movie/random"
-POSS_VAL_BY_FIELD = f"{LINK}movie/possible-values-by-field"
-MOVIE = f"{LINK}movie"
-SEASON = f"{LINK}season"
-REVIEW = f"{LINK}review"
-PERSON = f"{LINK}person"
-IMAGE = f"{LINK}image"
+LINK = "https://api.kinopoisk.dev"
+
+V1 = f"{LINK}/v1"
+V1_1 = f"{LINK}/v1.1"
+
+RANDOM = f"{V1}/movie/random"
+POSS_VAL_BY_FIELD = f"{V1}/movie/possible-values-by-field"
+
+MOVIE_AWARDS = f"{V1_1}/movie/awards"
+MOVIE = f"{V1}/movie"
+
+SEASON = f"{V1}/season"
+REVIEW = f"{V1}/review"
+
+PERSON_AWARDS = f"{V1_1}/person/awards"
+PERSON = f"{V1}/person/"
+
+STUDIO = f"{V1}/studio"
+KEYWORD = f"{V1}/keyword"
+IMAGE = f"{V1}/image"
